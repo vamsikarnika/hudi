75302f7dcf920167c56eef2566cec185314e0274
Lokesh Jain <ljain@apache.org>
2025-02-06 12:37:31 +0530

    Use config from Kafka

--- hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/helpers/KafkaSourceUtil.java
+++ hudi-utilities/src/main/java/org/apache/hudi/utilities/sources/helpers/KafkaSourceUtil.java
@@ -25,6 +25,7 @@ import org.apache.hudi.utilities.exception.HoodieReadFromSourceException;
 import org.apache.hudi.utilities.schema.SchemaProvider;
 
 import com.google.crypto.tink.subtle.Base64;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 import java.util.Objects;
 
@@ -32,7 +33,6 @@ import static org.apache.hudi.utilities.config.KafkaSourceConfig.KAFKA_VALUE_DES
 
 public class KafkaSourceUtil {
 
-  public static final String NATIVE_KAFKA_CONSUMER_GROUP_ID = "group.id";
   public static final int GROUP_ID_MAX_BYTES_LENGTH = 255;
 
   public static void configureSchemaDeserializer(SchemaProvider schemaProvider, TypedProperties props) {
@@ -41,10 +41,10 @@ public class KafkaSourceUtil {
     }
     props.put(KAFKA_VALUE_DESERIALIZER_SCHEMA.key(), schemaProvider.getSourceSchema().toString());
     // assign consumer group id based on the schema, since if there's a change in the schema we ensure KafkaRDDIterator doesn't use cached Kafka Consumer
-    String groupId = props.getString(NATIVE_KAFKA_CONSUMER_GROUP_ID, "");
+    String groupId = props.getString(ConsumerConfig.GROUP_ID_CONFIG, "");
     String schemaHash = Base64.encode(HashID.hash(schemaProvider.getSourceSchema().toString(), HashID.Size.BITS_128));
     String updatedConsumerGroup = groupId.isEmpty() ? schemaHash
         : StringUtils.concatenateWithThreshold(String.format("%s_", groupId), schemaHash, GROUP_ID_MAX_BYTES_LENGTH);
-    props.put(NATIVE_KAFKA_CONSUMER_GROUP_ID, updatedConsumerGroup);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, updatedConsumerGroup);
   }
 }
